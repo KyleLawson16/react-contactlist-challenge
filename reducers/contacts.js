c1f9e9a058b18de40621b5325db14d7cf89cf0f7
ppak10 <ppak10@gmail.com>
2020-04-01 10:20:16 -0700

    Adds sorting and extra test values to contacts list

--- reducers/contacts.js
+++ reducers/contacts.js
@@ -1,3 +1,34 @@
+/**
+ * contacts.js
+ * Reducers for contacts
+ */
+
+/**
+ * Sorts array by lastName value then firstName value if lastName is the same
+ * @param {Object} a 
+ * @param {Object} b 
+ */
+const sortLastNameFirstName = (a, b) => {
+  // Converts all to values to uppercase to ignore capitialization
+  const lastNameA = a.lastName.toUpperCase();
+  const lastNameB = b.lastName.toUpperCase();
+  const firstNameA = a.firstName.toUpperCase();
+  const firstNameB = b.firstName.toUpperCase();
+  if (lastNameA < lastNameB) {
+    return -1;
+  } else if (lastNameA > lastNameB) {
+    return 1;
+  } else {
+    if (firstNameA < firstNameB) {
+      return -1;
+    } else if (firstNameA > firstNameB) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+};
+
 const initialState = {
   data: [],
   isFetching: null,
@@ -17,7 +48,7 @@ export default function contacts(state = initialState, action) {
     case "FETCH_CONTACTS_COMPLETE":
       return {
         ...state,
-        data: action.data,
+        data: action.data.sort(sortLastNameFirstName),
         isFetching: false
       };
     case "FETCH_CONTACTS_FAILED":
--- utils/mockAPI.js
+++ utils/mockAPI.js
@@ -1,6 +1,22 @@
+/**
+ * mockAPI.js
+ * Example values to provide to the frontend
+ */
+
+const testValues = [
+  { firstName: "alex", lastName: "lane", phone: "5429873456" },
+  { firstName: "black", lastName: "smith", phone: "" },
+  { firstName: "rich", lastName: "walker", phone: "542737-3246" },
+  { firstName: "Alej", lastName: "Lexi", phone: "542-345-8721" },
+  { firstName: "Erik", lastName: "lexi", phone: "(542)321-3456" },
+  { firstName: "richardo", lastName: "julian", phone: "542 211 5678" },
+  { firstName: "bill", lastName: "Alex", phone: "542-6542154" }
+];
+
 export const resolveContacts = () =>
   new Promise(resolve => {
     const contacts = [
+      ...testValues,
       { firstName: "Allen", lastName: "Lane", phone: "542-987-3456" },
       { firstName: "Jane", lastName: "Smith", phone: "" },
       { firstName: "Richard", lastName: "Walker", phone: "542-737-3246" },
